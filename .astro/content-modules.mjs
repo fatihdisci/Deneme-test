3637552d84f554c3063d8646c0cb03d41b1772e9
Fatih Dişçi <fatihdisci@outlook.com>
2026-01-15 12:32:39 +0300

    content: ibo - 2026-01-15 12:32

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,15 @@
 
 export default new Map([
-["src/content/blog/deneem2323.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneem2323.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/asdasd.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fasdasd.mdx&astroContentModuleFlag=true")],
+["src/content/blog/bilgisayar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbilgisayar.mdx&astroContentModuleFlag=true")],
+["src/content/blog/deneem2323.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneem2323.mdx&astroContentModuleFlag=true")],
+["src/content/blog/deneme1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneme1.mdx&astroContentModuleFlag=true")],
+["src/content/blog/deneme.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneme.mdx&astroContentModuleFlag=true")],
+["src/content/blog/gecersiz-idari-islem-faiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgecersiz-idari-islem-faiz.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ilk-yazi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Filk-yazi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")],
+["src/content/blog/olunceye-kadar-bakim-muvazaa.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Folunceye-kadar-bakim-muvazaa.mdx&astroContentModuleFlag=true")],
+["src/content/blog/sozlesmeden-donme-tapu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsozlesmeden-donme-tapu.mdx&astroContentModuleFlag=true")],
+["src/content/blog/yonetici-primi-ispat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyonetici-primi-ispat.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ibo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fibo.mdx&astroContentModuleFlag=true")]]);
 		
