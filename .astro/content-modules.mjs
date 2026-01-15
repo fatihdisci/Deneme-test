983ad2ef0c314be56d1ebbf0f3766ec4d71a281b
Fatih Dişçi <fatihdisci@outlook.com>
2026-01-15 13:37:29 +0300

    content: format all MDX files with HTML tags, sync deleted files

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,8 @@
 
 export default new Map([
-["src/content/blog/deneem2323.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneem2323.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ibo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fibo.mdx&astroContentModuleFlag=true")],
-["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/gecersiz-idari-islem-faiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgecersiz-idari-islem-faiz.mdx&astroContentModuleFlag=true")],
+["src/content/blog/sozlesmeden-donme-tapu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsozlesmeden-donme-tapu.mdx&astroContentModuleFlag=true")],
+["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")],
+["src/content/blog/yonetici-primi-ispat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyonetici-primi-ispat.mdx&astroContentModuleFlag=true")],
+["src/content/blog/olunceye-kadar-bakim-muvazaa.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Folunceye-kadar-bakim-muvazaa.mdx&astroContentModuleFlag=true")]]);
 		
