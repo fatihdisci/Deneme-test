dc8fa8e2c85c605f31a2b3ff1f7a06c412ea6dba
Fatih Dişçi <fatihdisci@outlook.com>
2026-01-15 12:27:14 +0300

    fix: remove corrupted asdasd.mdx file to fix MDX build

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,4 @@
 
 export default new Map([
-["src/content/blog/asdasd.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fasdasd.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/deneem2323.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneem2323.mdx&astroContentModuleFlag=true")]]);
 		
