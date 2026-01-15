e9e0fd7891df187b13438875657084e1ecfcaea8
Fatih Dişçi <fatihdisci@outlook.com>
2026-01-15 12:42:44 +0300

    layout: justify blog post text and lists

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/blog/deneem2323.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeneem2323.mdx&astroContentModuleFlag=true")],
-["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ibo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fibo.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/ibo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fibo.mdx&astroContentModuleFlag=true")],
+["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")]]);
 		
