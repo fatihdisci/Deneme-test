ca5acc7fe9d6de46ecda7a7b3ef3c75802b097a4
Fatih Dişçi <fatihdisci@outlook.com>
2026-01-16 10:18:54 +0300

    chore: update site URL and CNAME for production deployment

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,17 +1,17 @@
 
 export default new Map([
+["src/content/blog/gecersiz-idari-islem-faiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgecersiz-idari-islem-faiz.mdx&astroContentModuleFlag=true")],
+["src/content/blog/olunceye-kadar-bakim-muvazaa.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Folunceye-kadar-bakim-muvazaa.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/muaraza-giderilmesi-sgk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmuaraza-giderilmesi-sgk.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/yonetici-primi-ispat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyonetici-primi-ispat.mdx&astroContentModuleFlag=true")],
-["src/content/blog/sozlesmeden-donme-tapu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsozlesmeden-donme-tapu.mdx&astroContentModuleFlag=true")],
-["src/content/blog/olunceye-kadar-bakim-muvazaa.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Folunceye-kadar-bakim-muvazaa.mdx&astroContentModuleFlag=true")],
 ["src/content/services/aile-ve-bosanma.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Faile-ve-bosanma.mdx&astroContentModuleFlag=true")],
-["src/content/services/bilisim-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fbilisim-hukuku.mdx&astroContentModuleFlag=true")],
+["src/content/blog/sozlesmeden-donme-tapu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsozlesmeden-donme-tapu.mdx&astroContentModuleFlag=true")],
 ["src/content/services/gayrimenkul-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fgayrimenkul-hukuku.mdx&astroContentModuleFlag=true")],
+["src/content/services/bilisim-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fbilisim-hukuku.mdx&astroContentModuleFlag=true")],
 ["src/content/services/icra-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Ficra-hukuku.mdx&astroContentModuleFlag=true")],
-["src/content/services/idare-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fidare-hukuku.mdx&astroContentModuleFlag=true")],
-["src/content/blog/gecersiz-idari-islem-faiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgecersiz-idari-islem-faiz.mdx&astroContentModuleFlag=true")],
+["src/content/services/ceza-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fceza-hukuku.mdx&astroContentModuleFlag=true")],
 ["src/content/services/is-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fis-hukuku.mdx&astroContentModuleFlag=true")],
-["src/content/services/sirket-danismanligi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fsirket-danismanligi.mdx&astroContentModuleFlag=true")],
 ["src/content/services/ticaret-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fticaret-hukuku.mdx&astroContentModuleFlag=true")],
-["src/content/services/ceza-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fceza-hukuku.mdx&astroContentModuleFlag=true")]]);
+["src/content/services/idare-hukuku.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fidare-hukuku.mdx&astroContentModuleFlag=true")],
+["src/content/services/sirket-danismanligi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fsirket-danismanligi.mdx&astroContentModuleFlag=true")]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://beta.discilaw.com',
+  site: 'https://www.discilaw.com',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
